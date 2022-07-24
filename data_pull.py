ed22b8906becf993355ca8be8f2e55f15c25c8aa
AmanShah9 <aman.17bec1034@abes.ac.in>
2022-07-24 12:47:41 +0530

    folder

--- data_pull.py
+++ data_pull.py
@@ -1,3 +1,5 @@
+
+
 def get_exchange_data() -> List[Dict[str, Any]]:
     url = 'https://meter-api.isocietymanager.com/v1/society/99/exportreading?api-token=3a980a33acd7b7d17954f8b422dd04d435f0d7d66af450b281ecd9e8c45f3934&user-id=112553&from_date=2022-06-11&to_date=2022-06-18&unit=AZJ0205&type=xls'
     try:
